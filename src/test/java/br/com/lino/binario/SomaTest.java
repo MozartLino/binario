3207a3b6cc05fb5dfef2ff6dff0b66c298e75317
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-08-15 14:02:06 -0300

    melhorando os testes

--- src/test/java/br/com/lino/binario/SomaTest.java
+++ src/test/java/br/com/lino/binario/SomaTest.java
@@ -15,51 +15,51 @@ public class SomaTest {
 
 	@Test
 	public void deveSomarValoresPares() {
-		int valor = soma.calcula("10", "10");
+		String valor = soma.calcula("10", "10");
 
-		assertEquals(100, valor);
+		assertEquals("100", valor);
 	}
 
 	@Test
 	public void deveSomarValoresImpares() {
-		int valor = soma.calcula("11", "11");
+		String valor = soma.calcula("11", "11");
 
-		assertEquals(110, valor);
+		assertEquals("110", valor);
 	}
 
 	@Test
 	public void deveSomarImparComPar() {
-		int valor = soma.calcula("11", "10");
+		String valor = soma.calcula("11", "10");
 
-		assertEquals(101, valor);
+		assertEquals("101", valor);
 	}
 
 	@Test
 	public void deveSomarParComImpar() {
-		int valor = soma.calcula("10", "11");
+		String valor = soma.calcula("10", "11");
 
-		assertEquals(101, valor);
+		assertEquals("101", valor);
 	}
 
 	@Test
 	public void deveSomarTamanhosIguais() {
-		int valor = soma.calcula("10", "10");
+		String valor = soma.calcula("10", "10");
 
-		assertEquals(100, valor);
+		assertEquals("100", valor);
 	}
 
 	@Test
 	public void deveSomarComTamanhoDoPrimeiroMaiorQueSegundo() {
-		int valor = soma.calcula("11001", "11");
+		String valor = soma.calcula("11001", "11");
 
-		assertEquals(11100, valor);
+		assertEquals("11100", valor);
 	}
 
 	@Test
 	public void deveSomarComTamanhoDoSegundoMaiorQuePrimeiro() {
-		int valor = soma.calcula("11", "11001");
+		String valor = soma.calcula("11", "11001");
 
-		assertEquals(11100, valor);
+		assertEquals("11100", valor);
 	}
 
 }
